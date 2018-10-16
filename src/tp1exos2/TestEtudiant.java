42ec3515ef6869953bab7e02ea3b38cb873f8084
myriamabdi <36502384+myriamabdi@users.noreply.github.com>
2018-10-16 10:32:23 +0200

    Update TestEtudiant.java

--- src/tp1exos2/TestEtudiant.java
+++ src/tp1exos2/TestEtudiant.java
@@ -1,4 +1,4 @@
-/*
+/* pour la java doc : C:\Users\33666\Documents\NetBeansProjects\tp1exos2\dist\javadoc\index.html
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
